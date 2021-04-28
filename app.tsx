6c4febbd61ea5a8ac251c2be994a8c81545e3724
alfredo.salzillo <noctisbels@gmai.com>
2021-04-28 15:41:07 +0200

    chore: added favicon

--- app.tsx
+++ app.tsx
@@ -8,6 +8,7 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
         <title>Alfredo Salzillo</title>
         <meta name="description" content="Alfredo Salzillo personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg" href="/favicon.svg"/>
         <script src="https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js" />
         <script src="https://www.gstatic.com/firebasejs/8.4.2/firebase-analytics.js" />
         <script src="/analytics.js" />
