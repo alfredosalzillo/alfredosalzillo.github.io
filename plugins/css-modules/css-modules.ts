e4bffd36f274e80cc1a7be8c2b4cf6b531d904dd
alfredosalzillo <a.salzillo@treedom.net>
2023-01-08 12:50:27 +0100

    chore: use deno sass instead of postcss compiler

--- plugins/css-modules/css-modules.ts
+++ plugins/css-modules/css-modules.ts
@@ -1,25 +1,22 @@
-import postcss from "postcss";
-import autoprefixer from "autoprefixer";
+import sass from "https://deno.land/x/denosass@1.0.6/mod.ts";
 import type { ModuleLoader, ModuleLoaderEnv, ModuleLoaderOutput } from "aleph/server/types.ts";
-import { parse, lex } from "https://deno.land/x/css@0.2.0/mod.ts";
+import { lex } from "https://deno.land/x/css@0.2.0/mod.ts";
 import * as parsel from './parsel.js'
-import { camelCase } from "https://deno.land/x/case/mod.ts";
-import { encode, decode } from "https://deno.land/std/encoding/base64.ts"
+import { camelCase } from "https://deno.land/x/case@2.1.1/mod.ts";
 import { basename } from "https://deno.land/std@0.170.0/path/mod.ts";
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import ShortUniqueId from 'https://cdn.jsdelivr.net/npm/short-unique-id@latest/short_uuid/mod.ts';
 
 const uid = new ShortUniqueId();
 
 
-const uniques = new Map<string, string>()
+const componentClassPrefixes = new Map<string, string>()
 const uniqueComponentClassPrefix = (specifier: string) => {
-  if (uniques.has(specifier)) {
-    return uniques.get(specifier)
+  if (componentClassPrefixes.has(specifier)) {
+    return componentClassPrefixes.get(specifier)
   }
   const [componentName] = basename(specifier).split('.')
   const componentClassPrefix = [componentName, uid(12)].join('_')
-  uniques.set(specifier, componentClassPrefix)
+  componentClassPrefixes.set(specifier, componentClassPrefix)
   return componentClassPrefix
 }
 
@@ -39,8 +36,8 @@ export class CssModuleLoader implements ModuleLoader {
   }
 
   async load(specifier: string, content: string, env: ModuleLoaderEnv): Promise<ModuleLoaderOutput> {
-    const ret = await postcss([autoprefixer]).process(content);
-    const selectors = lex(ret.css)
+    const compiledCss = sass(content).to_string('expanded') as string
+    const selectors = lex(compiledCss)
       .filter((token) => token.type === 'selector')
       .map((token) => token.text)
       .flatMap(parsel.tokenize)
@@ -52,7 +49,7 @@ export class CssModuleLoader implements ModuleLoader {
     }), {} as { [key: string]: string })
     const css = selectors.reduce((acc, name) => {
       return acc.replace(new RegExp(`\\.${name}\\b`, 'g'), `.${names[camelCase(name)]}`)
-    }, ret.css)
+    }, compiledCss)
     return {
       code: `export default ${JSON.stringify(names)};`,
       lang: "js",
