beb2e50df030fb5b2055ee191a5e5d42886a2475
alfredosalzillo <a.salzillo@treedom.net>
2023-01-08 13:07:57 +0100

    chore: code refactor

--- plugins/scss-loader/scss-loader.ts
+++ plugins/scss-loader/scss-loader.ts
@@ -1,14 +1,12 @@
 import sass from "https://deno.land/x/denosass@1.0.6/mod.ts";
 import type { ModuleLoader, ModuleLoaderEnv, ModuleLoaderOutput } from "aleph/server/types.ts";
 import { lex } from "https://deno.land/x/css@0.2.0/mod.ts";
-import * as parsel from './parsel.js'
 import { camelCase } from "https://deno.land/x/case@2.1.1/mod.ts";
 import { basename } from "https://deno.land/std@0.170.0/path/mod.ts";
 import ShortUniqueId from 'https://cdn.jsdelivr.net/npm/short-unique-id@latest/short_uuid/mod.ts';
+import * as parsel from './parsel.js'
 
 const uid = new ShortUniqueId();
-
-
 const componentClassPrefixes = new Map<string, string>()
 const uniqueComponentClassPrefix = (specifier: string) => {
   if (componentClassPrefixes.has(specifier)) {
@@ -19,6 +17,13 @@ const uniqueComponentClassPrefix = (specifier: string) => {
   componentClassPrefixes.set(specifier, componentClassPrefix)
   return componentClassPrefix
 }
+
+const extractClassNames = (css: string) => [...new Set(lex(css)
+  .filter((token) => token.type === 'selector')
+  .map((token) => token.text)
+  .flatMap(parsel.tokenize)
+  .filter((token: any) => token.type === 'class')
+  .map((token: any) => token.name as string) as string[])]
 
 export type CssModuleLoaderOptions = {
 
@@ -32,28 +37,23 @@ export class ScssLoader implements ModuleLoader {
   }
 
   test(path: string): boolean {
-    return path.endsWith(".module.css");
+    return path.endsWith(".module.css") || path.endsWith(".module.scss");
   }
 
   async load(specifier: string, content: string, env: ModuleLoaderEnv): Promise<ModuleLoaderOutput> {
-    const compiledCss = sass(content).to_string('expanded') as string
-    const selectors = lex(compiledCss)
-      .filter((token) => token.type === 'selector')
-      .map((token) => token.text)
-      .flatMap(parsel.tokenize)
-      .filter((token: any) => token.type === 'class')
-      .map((token: any) => token.name as string) as string[]
-    const names = selectors.reduce((acc, name) => ({
+    const compiledCSS = sass(content).to_string('expanded') as string
+    const classNames = extractClassNames(compiledCSS)
+    const uniqueClassNamesMap = classNames.reduce((acc, name) => ({
       ...acc,
       [camelCase(name)]: `${uniqueComponentClassPrefix(specifier)}__${name}`,
     }), {} as { [key: string]: string })
-    const css = selectors.reduce((acc, name) => {
-      return acc.replace(new RegExp(`\\.${name}\\b`, 'g'), `.${names[camelCase(name)]}`)
-    }, compiledCss)
+    const inlineCSS = classNames.reduce((acc, name) => {
+      return acc.replace(new RegExp(`\\.${name}\\b`, 'g'), `.${uniqueClassNamesMap[camelCase(name)]}`)
+    }, compiledCSS)
     return {
-      code: `export default ${JSON.stringify(names)};`,
+      code: `export default ${JSON.stringify(uniqueClassNamesMap)};`,
       lang: "js",
-      inlineCSS: css,
+      inlineCSS,
     };
   }
 }
