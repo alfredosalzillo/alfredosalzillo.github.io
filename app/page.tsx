5c6e55141a7afad80d9db97401773bcf5647acd1
alfredosalzillo <noctisbels@gmail.com>
2023-04-15 19:46:20 +0200

    feat(home): updated desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,40 @@ import Twitter from '@/icons/Twitter';
 import Github from '@/icons/Github';
 import React from 'react';
 import { fetchData } from '@/lib/data';
+import classes from './page.module.css'
 
 export default async function Home() {
   const { articles, repositories } = await fetchData();
   return (
     <main>
       <Header />
-      <section>
-        <ProfileCard />
-      </section>
-      <section>
-        <h2>Articles</h2>
-        {articles.map((article) => (
-          <ArticlePreview
-            key={article.url}
-            {...article}
-          />
-        ))}
-      </section>
-      <section>
-        <h2>Projects</h2>
-        {repositories.map((repository) => (
-          <RepositoryPreview
-            key={repository.url}
-            {...repository}
-          />
-        ))}
-      </section>
+      <div className={classes.content}>
+        <div className={classes.infos}>
+          <section>
+            <ProfileCard />
+          </section>
+        </div>
+        <div className={classes.main}>
+          <section>
+            <h2>Articles</h2>
+            {articles.map((article) => (
+              <ArticlePreview
+                key={article.url}
+                {...article}
+              />
+            ))}
+          </section>
+          <section>
+            <h2>Projects</h2>
+            {repositories.map((repository) => (
+              <RepositoryPreview
+                key={repository.url}
+                {...repository}
+              />
+            ))}
+          </section>
+        </div>
+      </div>
       <hr />
       <footer>
         <a href="https://www.linkedin.com/in/alfredosalzillo/" target="_blank">
