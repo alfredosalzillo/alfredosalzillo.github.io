e67aa65096a7dcec4f5027c47163934b726a7458
a.salzillo <noctisbels@gmail.com>
2026-02-07 14:31:02 +0100

    Refactor header and icons by replacing `SocialLinks` and individual SVG components with Material-UI icons and updated layout. Remove unused components like `ProfileCard`.

--- app/@header/default.tsx
+++ app/@header/default.tsx
@@ -1,7 +1,14 @@
+"use client";
+import NextLink from "next/link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { IconButton } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
 import Logo from "@/components/Logo";
-import SocialLinks from "@/components/SocialLinks";
+import DevIcon from "@/icons/Dev";
 
 const Header = () => {
   return (
@@ -11,10 +18,45 @@ const Header = () => {
         sx={{
           display: "flex",
           justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
         <Logo />
-        <SocialLinks />
+        <Stack direction="row" gap={0} alignItems="center">
+          <Button
+            variant="text"
+            component={NextLink}
+            href="https://alfredosalzillo.me/resume/alfredo-salzillo-resume.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </Button>
+          <IconButton
+            component={NextLink}
+            href="https://www.linkedin.com/in/alfredosalzillo/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            component={NextLink}
+            href="https://github.com/alfredosalzillo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon fontSize="small" />
+          </IconButton>
+          <IconButton
+            component={NextLink}
+            href="https://dev.to/alfredosalzillo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <DevIcon fontSize="small" />
+          </IconButton>
+        </Stack>
       </Toolbar>
     </AppBar>
   );
